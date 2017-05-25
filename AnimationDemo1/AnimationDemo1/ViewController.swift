e0094ec4d45cbd1b94f7160ac45fd216ec5e7fa3
duzhe <363958265@qq.com>
2017-05-25 10:17:36 +0800

    更新支持swift3 使用更swifty的方式 增加注释

--- AnimationDemo1/AnimationDemo1/ViewController.swift
+++ AnimationDemo1/AnimationDemo1/ViewController.swift
@@ -9,84 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var v1:UIView!
-    @IBOutlet weak var v2:UIView!
-    @IBOutlet weak var v3:UIView!
-    @IBOutlet weak var v4:UIView!
-    @IBOutlet weak var v5:UIView!
-    @IBOutlet weak var v6:UIView!
+  
+  @IBOutlet weak var v1:UIView!
+  @IBOutlet weak var v2:UIView!
+  @IBOutlet weak var v3:UIView!
+  @IBOutlet weak var v4:UIView!
+  @IBOutlet weak var v5:UIView!
+  @IBOutlet weak var v6:UIView!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    v2.alpha = 0
+  }
+  override func viewDidAppear(_ animated: Bool) {
+ 
+    // 普通的动画 v1
+    UIView.animate(withDuration: 1, delay: 0.2, options: [.repeat,.autoreverse,.curveEaseIn] , animations: {
+      self.v1.center.x += self.view.bounds.width
+    }, completion: nil)
     
+    // 弹簧动画
+    UIView.animate(withDuration: 1.2, delay: 0.3, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [.repeat,.autoreverse,.curveEaseIn] , animations: { () -> Void in
+      self.v2.center.x -= 100
+      self.v2.alpha = 1
+    }, completion: nil)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func viewWillAppear(animated: Bool) {
-       v2.alpha = 0
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-       
-        //普通动画
-        UIView.animateWithDuration(1, delay: 0.2, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.Repeat.rawValue | UIViewAnimationOptions.Autoreverse.rawValue | UIViewAnimationOptions.CurveEaseIn.rawValue) , animations: {
-            
-            self.v1.center.x += self.view.bounds.width
-            
-        }, completion: nil)
-        
-        UIView.animateWithDuration(1.2, delay: 0.3, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.Repeat.rawValue | UIViewAnimationOptions.Autoreverse.rawValue), animations: { () -> Void in
-                self.v2.center.x -= 100
-                self.v2.alpha = 1
-            
-            }, completion: nil)
-        
-        UIView.animateWithDuration(1.3, delay: 8, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.AllowAnimatedContent.rawValue ), animations: { () -> Void in
-            
-                 self.v3.transform = CGAffineTransformMakeTranslation(0, 100)
-            
-                 self.v3.transform = CGAffineTransformMakeScale(1.5, 1.5)
-                self.v3.backgroundColor = UIColor.yellowColor()
-            }) { _  in
-                
-              UIView.animateWithDuration(0.8, animations: {
-                 self.v3.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
-                
-              })
-        }
-        
-        let newView = UIView(frame: v4.bounds)
-        newView.backgroundColor = UIColor.redColor()
-        //过渡
-        UIView.transitionWithView(self.v4, duration: 2, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.Repeat.rawValue | UIViewAnimationOptions.TransitionFlipFromBottom.rawValue | UIViewAnimationOptions.CurveEaseIn.rawValue ), animations: { () -> Void in
-            self.v4.addSubview(newView);
-            }, completion: nil)
-        
-        
-        let newView1 = UIView(frame: v5.bounds)
-        newView1.backgroundColor = UIColor.redColor()
-        UIView.transitionWithView(self.v5, duration: 2, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.Repeat.rawValue | UIViewAnimationOptions.TransitionCurlDown.rawValue | UIViewAnimationOptions.CurveEaseIn.rawValue ), animations: { () -> Void in
-            self.v5.addSubview(newView1);
-            }, completion: nil)
-        
-        let newView2 = UIView(frame: v6.bounds)
-        newView2.backgroundColor = UIColor.redColor()
-        newView2.center = self.v6.center
-        
-        //这个感觉没啥效果呀。。
-//        UIView.transitionWithView(v6, duration: 2, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.Repeat.rawValue | UIViewAnimationOptions.TransitionFlipFromBottom.rawValue | UIViewAnimationOptions.CurveEaseOut.rawValue ), animations: { () -> Void in
-//                newView2.removeFromSuperview()
-//            }, completion: nil)
-      
-//        UIView.transitionFromView(self.v6, toView: newView2, duration:2, options: UIViewAnimationOptions(rawValue: UIViewAnimationOptions.Repeat.rawValue | UIViewAnimationOptions.TransitionFlipFromTop.rawValue  ), completion: nil)
-        
+    // transform
+    UIView.animate(withDuration: 1.3, delay: 0.1 , usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [.repeat,.autoreverse] , animations: { () -> Void in
+      self.v3.transform = CGAffineTransform
+        .identity
+        .translatedBy(x: 0, y: 5)
+        .scaledBy(x: 1.5, y: 1.5)
+      self.v3.backgroundColor = UIColor.yellow
+    }) { _  in
+      UIView.animate(withDuration: 0.8, animations: {
+        self.v3.transform = CGAffineTransform
+          .identity
+          .rotated(by: CGFloat(Double.pi))
+      })
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    let newView = UIView(frame: v4.bounds)
+    newView.backgroundColor = UIColor.red
+    // 过渡 transition 动画 可以修改属性 看看不同效果
+    UIView.transition(with: self.v4, duration: 2 , options: [.repeat,.transitionFlipFromBottom,.curveEaseIn] , animations: { () -> Void in
+      self.v4.addSubview(newView)  // 新增view会有过渡效果
+    }, completion: nil)
+    
+    
+    let newView1 = UIView(frame: v5.bounds)
+    newView1.backgroundColor = UIColor.red
+    // 另一个过渡动画
+    UIView.transition(with: self.v5, duration: 2, options: [.repeat,.transitionCurlDown ,.curveEaseIn] , animations: { () -> Void in
+      self.v5.addSubview(newView1)
+    }, completion: nil)
+    
+    let newView2 = UIView(frame: v6.bounds)
+    newView2.backgroundColor = UIColor.red
+    newView2.center = self.v6.center
+    // 过渡动画
+    UIView.transition(with: self.v6 , duration: 2, options: [.repeat,.transitionFlipFromTop ,.curveEaseIn] , animations: { () -> Void in
+      self.v6.addSubview(newView2)
+    }, completion: nil)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
 }
